f2e2c27644c5473da136e8294f4c4cbd3f044b14
Darwante, Sanket Atmaram <sdarwant@iu.edu>
2022-09-10 18:07:55 -0400

    basic structure

---
+++ search-engine-api/api-controller.py
@@ -0,0 +1,50 @@
+from starlette.requests import Request
+from starlette.responses import Response
+
+import weatherApi as weatherApi
+from fastapi import FastAPI, HTTPException
+import uvicorn
+from fastapi.responses import FileResponse
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+middleware = [Middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], )
+]
+
+app = FastAPI(middleware=middleware)
+
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception:
+        return Response("Internal server error", status_code=505)
+
+
+app.middleware('http')(catch_exceptions_middleware)
+
+
+@app.get("/search-api")
+async def search_api(search_text):
+    search_service = SearchService()
+    return search_service.filterData(search_text)
+
+
+
+def setup():
+    print('welcome to search engine api')
+    uvicorn.run(app, host="0.0.0.0", port=4600)
+
+
+
+if __name__ == '__main__':
+    setup()
---
+++ search-engine-api/search-service.py
@@ -0,0 +1,30 @@
+import csv
+import json
+
+class SearchService:
+
+    def csv_to_json(self, csvFilePath):
+        jsonArray = []
+        
+        #read csv file
+        with open(csvFilePath, encoding='utf-8') as csvf: 
+            #load csv file data using csv library's dictionary reader
+            csvReader = csv.DictReader(csvf) 
+
+            #convert each csv row into python dict
+            for row in csvReader: 
+                #add this python dict to json array
+                jsonArray.append(row)
+
+        return jsonArray
+
+    # csvFilePath = r'data.csv'
+    # # jsonFilePath = r'data.json'
+    # csv_to_json(csvFilePath)
+
+    def filterData(self, search_text):
+        print('Search String' + search_text)
+        jsonArray = self.csv_to_json('data.csv')
+        return jsonArray
+
+
